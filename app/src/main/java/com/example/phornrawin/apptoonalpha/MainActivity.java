388f7422ce3d201b406f086b9978328d698a1fa9
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-12-29 11:59:15 +0700

    add tabIcon to framework

--- app/src/main/java/com/example/phornrawin/apptoonalpha/MainActivity.java
+++ app/src/main/java/com/example/phornrawin/apptoonalpha/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.phornrawin.apptoonalpha;
 
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -10,14 +11,18 @@ import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.DynamicDrawableSpan;
 import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import com.example.phornrawin.apptoonalpha.fragment.DairyTableFragment;
 import com.example.phornrawin.apptoonalpha.fragment.HomeFragment;
 import com.example.phornrawin.apptoonalpha.fragment.MyToonFragment;
 import com.example.phornrawin.apptoonalpha.fragment.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
     private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private int[] icons = {R.drawable.home, R.drawable.calendar, R.drawable.like, R.drawable.user};
     private Drawable drawable;
 
 
@@ -29,22 +34,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialize();
+        createTabIcons();
     }
 
     public void initialize(){
         viewPager = findViewById(R.id.viewpage_main);
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public CharSequence getPageTitle(int position) {
-                if(position == 0){
-                    drawable = getResources().getDrawable(R.drawable.home);
-                }else if (position == 1) {
-                    drawable = getResources().getDrawable(R.drawable.calendar);
-                }else if (position == 2){
-                    drawable = getResources().getDrawable(R.drawable.like);
-                }else if (position == 3){
-                    drawable = getResources().getDrawable(R.drawable.user);
-                }
+//                if(position == 0){
+//                    drawable = getResources().getDrawable(R.drawable.home);
+//                }else if (position == 1) {
+//                    drawable = getResources().getDrawable(R.drawable.calendar);
+//                }else if (position == 2){
+//                    drawable = getResources().getDrawable(R.drawable.like);
+//                }else if (position == 3){
+//                    drawable = getResources().getDrawable(R.drawable.user);
+//                }
+                drawable = getBaseContext().getDrawable(icons[position]);
                 SpannableStringBuilder sb = new SpannableStringBuilder("    ");
                 drawable.setBounds(5, 5, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
                 ImageSpan span = new ImageSpan(drawable, DynamicDrawableSpan.ALIGN_BASELINE);
@@ -72,6 +81,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void createTabIcons() {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.home, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.calendar, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.like, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+
+        TextView tabFour = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabFour.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.user, 0, 0);
+        tabLayout.getTabAt(3).setCustomView(tabFour);
+    }
 
 
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
