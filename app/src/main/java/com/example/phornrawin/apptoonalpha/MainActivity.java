d1b190163cb73d1e59799c27d1fc878cb7a2cb86
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2018-01-02 22:04:13 +0700

    fix bug Icon

--- app/src/main/java/com/example/phornrawin/apptoonalpha/MainActivity.java
+++ app/src/main/java/com/example/phornrawin/apptoonalpha/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.phornrawin.apptoonalpha;
 
 import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.DynamicDrawableSpan;
@@ -19,9 +23,11 @@ import com.example.phornrawin.apptoonalpha.fragment.HomeFragment;
 import com.example.phornrawin.apptoonalpha.fragment.MyToonFragment;
 import com.example.phornrawin.apptoonalpha.fragment.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private ImageSpan imageSpan;
+    private SpannableStringBuilder sb;
     private int[] icons = {R.drawable.home, R.drawable.calendar, R.drawable.like, R.drawable.user};
     private Drawable drawable;
 
@@ -33,32 +39,41 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialize();
-        createTabIcons();
-    }
-
-    public void initialize(){
         viewPager = findViewById(R.id.viewpage_main);
         tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        initialize();
+        tabLayout.getTabAt(0).setIcon(icons[0]);
+        tabLayout.getTabAt(1).setIcon(icons[1]);
+        tabLayout.getTabAt(2).setIcon(icons[2]);
+        tabLayout.getTabAt(3).setIcon(icons[3]);
+//        createTabIcons();
+    }
+
+    public void initialize(){
+
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public CharSequence getPageTitle(int position) {
-//                if(position == 0){
-//                    drawable = getResources().getDrawable(R.drawable.home);
-//                }else if (position == 1) {
-//                    drawable = getResources().getDrawable(R.drawable.calendar);
-//                }else if (position == 2){
-//                    drawable = getResources().getDrawable(R.drawable.like);
-//                }else if (position == 3){
-//                    drawable = getResources().getDrawable(R.drawable.user);
+////                if(position == 0){
+////                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.home);
+////                }else if (position == 1) {
+////                    drawable =ContextCompat.getDrawable(MainActivity.this, R.drawable.calendar);
+////                }else if (position == 2){
+////                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.like);
+////                }else if (position == 3){
+////                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.user);
+////                }
+//                drawable =  MainActivity.this.getResources().getDrawable(icons[position]);
+//                sb = new SpannableStringBuilder("    ");
+//                drawable.setBounds(5, 5, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+//                imageSpan = new ImageSpan(drawable);
+//                sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//                for (int i = 0; i < tabLayout.getTabCount(); i++) {
+//                    tabLayout.getTabAt(i).setIcon(icons[i]);
 //                }
-                drawable = getBaseContext().getDrawable(icons[position]);
-                SpannableStringBuilder sb = new SpannableStringBuilder("    ");
-                drawable.setBounds(5, 5, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-                ImageSpan span = new ImageSpan(drawable, DynamicDrawableSpan.ALIGN_BASELINE);
-                sb.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                return sb;
+
+                return null;
             }
 
             @Override
@@ -81,10 +96,11 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     private void createTabIcons() {
 
         TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
-        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.home, 0, 0);
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.drawable.home);
         tabLayout.getTabAt(0).setCustomView(tabOne);
 
         TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
@@ -100,21 +116,4 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         tabLayout.getTabAt(3).setCustomView(tabFour);
     }
 
-
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-
-
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
 }
